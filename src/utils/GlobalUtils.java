21c53e3302b1c0958b58b8efe1ed57078b81b946
khovan <minhpnq1807@gmail.com>
2024-10-12 00:19:30 +0700

    feat: restruct utils package, add comparator package, add method filter to repository

--- src/utils/GlobalUtils.java
+++ src/utils/GlobalUtils.java
@@ -3,34 +3,28 @@ package utils;
 
 import java.util.Date;
 
-public class GlobalUtils implements IGlobalUtils{
+public class GlobalUtils{
 
-    @Override
     public String getValue(String label, String messageError) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
     public String getDouble(String label, String messageError) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
     public String getDate(String label, String messageError) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
     public String getInteger(String label, String messageError) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
     public String dateFormat(Date date) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
     public Date dateParse(String string) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
--- src/utils/course/ICourseUtils.java
+++
@@ -1,9 +0,0 @@
-package utils.course;
-
-import utils.Utils;
-
-public interface ICourseUtils extends Utils {
-
-    @Override
-    boolean validID(String id);
-}
