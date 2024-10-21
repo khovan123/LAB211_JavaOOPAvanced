1904cfb0edb4f093ec26ec076a1f274de4b1e658
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2024-10-21 10:14:13 +0700

    Update Repository.java

--- src/repository/interfaces/Repository.java
+++ src/repository/interfaces/Repository.java
@@ -10,12 +10,4 @@ public interface Repository<T, C> {
 
     void writeFile(C entry) throws IOException;
 
-    void add(T entry) throws EventException, InvalidDataException;
-
-    void delete(String id) throws EventException;
-
-    T search(Predicate<T> p) throws NotFoundException;
-
-    T filter(String entry, String regex) throws InvalidDataException;
-
 }
