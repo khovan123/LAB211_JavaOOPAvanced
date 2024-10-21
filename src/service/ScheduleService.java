7bc90e46079a2cb6a56e5f6584decd5204d2ee4e
duyenvtt1414 <vothuyduyen1414@gmail.com>
2024-10-21 14:24:45 +0700

    Save local changes to ScheduleService.java

--- src/service/ScheduleService.java
+++ src/service/ScheduleService.java
@@ -11,8 +11,18 @@ import java.util.function.Predicate;
 import model.Schedule;
 
 public class ScheduleService implements IScheduleService {
+
+    List<Schedule> scheduleList = new ArrayList<>();
+
     @Override
     public void display() throws EmptyDataException {
+        if (scheduleList.isEmpty()) {
+            throw new EmptyDataException("Schedule is empty.");
+        } else {
+            for (Schedule s : scheduleList) {
+                System.out.println(s);
+            }
+        }
     }
 
     @Override
@@ -35,7 +45,4 @@ public class ScheduleService implements IScheduleService {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    
-
-    
 }
