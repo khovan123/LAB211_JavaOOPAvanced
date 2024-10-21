456d36d999c209db9507c01eec3b781771e82ab3
huyDev <hyperrhy1412@gmail.com>
2024-10-21 13:05:39 +0700

    fix CourseRepository

--- src/repository/CourseRepository.java
+++ src/repository/CourseRepository.java
@@ -3,28 +3,16 @@ package repository;
 
 import exception.EventException;
 import exception.IOException;
-import exception.InvalidDataException;
-import exception.NotFoundException;
+
+
 import model.Course;
 import repository.interfaces.ICourseRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 
-public class CourseRepository implements ICourseRepository{
-    private static List<Course> courses =  new ArrayList<>();
-
-    public CourseRepository() {}
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public static void setCourses(List<Course> courses) {
-        CourseRepository.courses = courses;
-    }
+public class CourseRepository implements ICourseRepository {
 
     public List<Course> readFile() throws IOException {
         return new ArrayList<>();
