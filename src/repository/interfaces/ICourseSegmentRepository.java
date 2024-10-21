1ca0fe9a8800b9170b5cfb596c040e55c1558bf6
khovan <minhpnq1807@gmail.com>
2024-10-21 17:59:57 +0700

    fix: interface service & repository

--- src/repository/interfaces/ICourseSegmentRepository.java
+++ src/repository/interfaces/ICourseSegmentRepository.java
@@ -1,22 +1,10 @@
 package repository.interfaces;
 
-import exception.EventException;
-import exception.IOException;
-import exception.InvalidDataException;
-import exception.NotFoundException;
 import java.util.List;
-import java.util.function.Predicate;
 import model.CourseSegment;
 
 public interface ICourseSegmentRepository extends Repository<CourseSegment, List<CourseSegment>> {
 
     final String path = "";
 
-    
-
-    List<CourseSegment> readFile() throws IOException;
-
-    void writeFile(List<CourseSegment> courseSegments) throws IOException;
-
-    
 }
--- src/repository/interfaces/IUserRepository.java
+++ src/repository/interfaces/IUserRepository.java
@@ -1,22 +1,9 @@
 package repository.interfaces;
 
-import exception.EventException;
-import exception.IOException;
-import exception.InvalidDataException;
-import exception.NotFoundException;
 import java.util.List;
-import java.util.function.Predicate;
 import model.User;
 
 public interface IUserRepository extends Repository<User, List<User>> {
 
     final String userPath = "";
-
-    @Override
-    List<User> readFile() throws IOException;
-
-    @Override
-    void writeFile(List<User> entry) throws IOException, java.io.IOException;
-
-   
 }
