e1291b748624d226b1eef1212da5c5e09f1dee16
huyDev <hyperrhy1412@gmail.com>
2024-10-24 07:25:00 +0700

    update Feat Readfile CourseRepo

--- src/repository/CourseRepository.java
+++ src/repository/CourseRepository.java
@@ -2,14 +2,43 @@ package repository;
 
 import exception.IOException;
 import model.Course;
+import model.Workout;
 import repository.interfaces.ICourseRepository;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CourseRepository implements ICourseRepository {
 
     @Override
     public List<Course> readFile() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new IOException("-> File Not Found - " + path);
+        }
+
+        List<Course> courseList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                try {
+                    Course course = new Course(
+                            data[0],
+                            data[1],
+                            data[2]);
+                    courseList.add(course);
+                } catch (Exception e) {
+                    throw new IOException("-> Error While Adding Course " + e.getMessage());
+                }
+            }
+        } catch (java.io.IOException e) {
+            throw new RuntimeException("-> Error While Reading Course Data " + e.getMessage());
+        }
+        return courseList;
     }
 
     @Override
