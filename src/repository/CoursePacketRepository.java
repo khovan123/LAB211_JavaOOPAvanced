036489fdf2dc3b84a541aea78a93b12fdaab31e6
huyDev <hyperrhy1412@gmail.com>
2024-10-24 07:54:25 +0700

    update Feat Readfile Coursepacket Repository

--- src/repository/CoursePacketRepository.java
+++ src/repository/CoursePacketRepository.java
@@ -1,15 +1,50 @@
 package repository;
 
 import exception.IOException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
+
 import model.CoursePacket;
+import model.CourseSegment;
 import repository.interfaces.ICoursePacketRepository;
 
 public class CoursePacketRepository implements ICoursePacketRepository {
 
     @Override
     public List<CoursePacket> readFile() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<CoursePacket> coursePacketList = new ArrayList<>();
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new IOException("-> File not found");
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+//              List<CourseSegment> courseSegmentList = new ArrayList<>();
+//              for (int i = 3; i < data.length ; i++) {
+//                  courseSegmentList.add(new CourseSegment(data[i]));
+//              }
+                try {
+                    CoursePacket coursePacket = new CoursePacket(
+                            data[0],
+                            data[1],
+                            data[2]
+                    );
+                    coursePacketList.add(coursePacket);
+                } catch (Exception e) {
+                    throw new IOException("-> Error While Adding - " + e.getMessage());
+                }
+            }
+        } catch (java.io.IOException e) {
+            throw new IOException("-> Error While Reading File - " + e.getMessage());
+        }
+        return coursePacketList;
     }
 
     @Override
