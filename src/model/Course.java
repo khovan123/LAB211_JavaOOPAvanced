4716a4aaf3a9f2990de1771b4c5a94c2a9423cf6
huyDev <hyperrhy1412@gmail.com>
2024-10-28 00:06:59 +0700

    Update runValidate feat

--- src/model/Course.java
+++ src/model/Course.java
@@ -2,89 +2,48 @@ package model;
 
 import exception.InvalidDataException;
 import service.WorkoutService;
+import utils.GlobalUtils;
+import utils.ObjectUtils;
 
-import java.time.LocalDate;
+import java.text.ParseException;
+import java.util.Date;
 
-//this class store default course data, can not be change when coursepacket data change
 public class Course {
 
     private String courseId;
     private String courseName;
     private boolean addventor;
-    private LocalDate generateDate;
+    private Date generateDate;
     private double price;
     private String comboID;
     private String coachId;
-    WorkoutService workoutService; //all workout, to know what you do in course
+    WorkoutService workoutService; // All workouts, to know what you do in the course
 
-    public Course(String courseId, String courseName, String coachId, WorkoutService workoutService) {
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.coachId = coachId;
-        this.workoutService = workoutService;
-    }
-
-    public Course(String courseId, String courseName, String coachId) {
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.coachId = coachId;
-    }
-
-    public Course(String courseId, String courseName, boolean addventor, LocalDate generateDate, double price, String comboID, String coachId, WorkoutService workoutService) {
+    public Course(String courseId, String courseName, boolean addventor, String generateDate, double price, String comboID, String coachId) throws InvalidDataException {
         this.courseId = courseId;
         this.courseName = courseName;
         this.addventor = addventor;
-        this.generateDate = generateDate;
-        this.price = price;
-        this.comboID = comboID;
-        this.coachId = coachId;
-        this.workoutService = workoutService;
-    }
-
-    public Course(String courseId, String courseName, boolean addventor, LocalDate generateDate, double price, String comboID, String coachId) {
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.addventor = addventor;
-        this.generateDate = generateDate;
+        this.setGenerateDate(generateDate);
         this.price = price;
         this.comboID = comboID;
         this.coachId = coachId;
     }
 
-    public Course(Course course) {
-
+    public Course(String courseId, String courseName, boolean addventor, String generateDate, double price, String comboID, String coachId, WorkoutService workoutService) throws InvalidDataException {
+        this(courseId, courseName, addventor, generateDate, price, comboID, coachId); // Call primary constructor
+        this.workoutService = workoutService;
     }
 
-    public String getCourseId() {
-        return courseId;
+    public void setGenerateDate(String generateDate) throws InvalidDataException {
+        try {
+            this.generateDate = GlobalUtils.getDate(generateDate);
+        } catch (ParseException e) {
+            throw new InvalidDataException("-> Generate Date Must Be dd/MM/yyyy");
+        }
     }
 
-    public void setCourseId(String courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public boolean getAddventor() {
-        return addventor;
-    }
-
-    public void setAddventor(boolean addventor) {
-        this.addventor = addventor;
-    }
-
-    public LocalDate getGenerateDate() {
-        return generateDate;
-    }
-
-    public void setGenerateDate(LocalDate generateDate) {
-        this.generateDate = generateDate;
+    public Date getGenerateDate() {
+        return this.generateDate;
     }
 
     public double getPrice() {
@@ -119,11 +78,41 @@ public class Course {
         this.workoutService = workoutService;
     }
 
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public boolean isAddventor() {
+        return addventor;
+    }
+
+    public void setAddventor(boolean addventor) {
+        this.addventor = addventor;
+    }
+
     public String getInfo() {
-        return String.format("", "");
+        return String.format("Course ID: %s, Course Name: %s, Adventor: %b, Generate Date: %s, Price: %.2f, Combo ID: %s, Coach ID: %s",
+                courseId, courseName, addventor, GlobalUtils.getDateString(generateDate), price, comboID, coachId);
     }
 
     public void runValidate() throws InvalidDataException {
-
+        if (!ObjectUtils.validCourseID(courseId)) {
+            throw new InvalidDataException("-> Course ID Must Be CS-YYYY.");
+        }
+        if (!ObjectUtils.validCoursePrice(String.valueOf(price))) {
+            throw new InvalidDataException("-> Price Must Be Larger Than 0.");
+        }
     }
 }
--- src/utils/ObjectUtils.java
+++ src/utils/ObjectUtils.java
@@ -6,4 +6,15 @@ public class ObjectUtils {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    public static boolean validCourseID(String code){
+        return code.matches("^CS\\d*$");
+    }
+
+    public static boolean validCoursePrice(String coursePrice){
+        try {
+            return Double.parseDouble(coursePrice) > 0;
+        } catch (NumberFormatException e){
+            return false;
+        }
+    }
 }
